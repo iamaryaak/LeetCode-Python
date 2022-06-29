2a7ce7a3ec0a6aba7dbc2bc86e312a8af3ed841b
Arya Kulkarni <kulkarya@gmail.com>
2022-06-28 20:09:12 -0400

    June 29

--- MaximumSubarray.py
+++ MaximumSubarray.py
@@ -1,6 +1,4 @@
-for i in range(1, len(nums)):
-            nums[i] = max(nums[i - 1] + nums[i], nums[i])
-        return max(nums)class Solution:
+class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         for i in range(1, len(nums)):
             nums[i] = max(nums[i - 1] + nums[i], nums[i])
