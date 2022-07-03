e9f8b55eb417ee5d90d5b41a0e014ddfbe834dc1
Arya Kulkarni <kulkarya@gmail.com>
2022-07-02 23:31:15 -0400

    July 2

---
+++ 19.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # first idea is to reverse a linked list (but that would take longer)
+        # create dummy variable
+        dummy = ListNode(0, head)
+        # choose 2 pointers
+        left = dummy
+        right = head
+        
+        # set right pointer to be n ahead of left pointer
+        while n > 0 and right:
+            right = right.next
+            n -= 1
+        
+        # iterate until right is at the end of the list and return 
+        while right:
+            left = left.next
+            right = right.next
+        # left pointer is at the node thats supposed to be deleted
+        left.next = left.next.next
+        return dummy.next
