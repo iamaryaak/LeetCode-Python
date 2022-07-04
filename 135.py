d10bb0dc12f6a0c38e19be686647c6663a15c7ef
Arya Kulkarni <kulkarya@gmail.com>
2022-07-04 09:49:01 -0400

    July 4

--- 135.py
+++ 135.py
@@ -1,17 +1,22 @@
-# space = O(n)
-# time = O(n)
+# Space = O(n)
+# Time = O(n)
 
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         # sort the ratings
         # start with +1 for each space 
         candiesPer = [1 for x in ratings]
+        
+        # for increasing slope
         for n in range(1, len(ratings)):
             # if rating is greater than
             if ratings[n]  > ratings[n-1]:
                 candiesPer[n] = max(candiesPer[n-1] + 1, candiesPer[n])
+                
+        # for decreasing slope
         for n in range(len(ratings)-2, -1, -1):
             if ratings[n]  > ratings[n+1]:
                 candiesPer[n] = max(candiesPer[n+1] + 1, candiesPer[n])
             
+        # return the sum since candiesPer tracks candies per person
         return sum(candiesPer)
