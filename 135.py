959cd142b1ecf4103b19d0db505eb5d6ed7f0542
Arya Kulkarni <kulkarya@gmail.com>
2022-07-04 09:41:11 -0400

    July 4

--- 135.py
+++ 135.py
@@ -3,68 +3,11 @@ class Solution:
         # sort the ratings
         # start with +1 for each space 
         candiesPer = [1 for x in ratings]
-        N = len(ratings)
-        for n in range(1, N):
+        for n in range(1, len(ratings)):
             # if rating is greater than
             if ratings[n]  > ratings[n-1]:
                 candiesPer[n] = max(candiesPer[n-1] + 1, candiesPer[n])
-        for n in range(N-2, -1, -1):
-            if ratings[n]  > ratings[n+1]:
-                candiesPer[n] = max(candiesPer[n+1] + 1, candiesPer[n])
-            
-        return sum(candiesPer)class Solution:
-    def candy(self, ratings: List[int]) -> int:
-        # sort the ratings
-        # start with +1 for each space 
-        candiesPer = [1 for x in ratings]
-        N = len(ratings)
-        for n in range(1, N):
-            # if rating is greater than
-            if ratings[n]  > ratings[n-1]:
-                candiesPer[n] = max(candiesPer[n-1] + 1, candiesPer[n])
-        for n in range(N-2, -1, -1):
-            if ratings[n]  > ratings[n+1]:
-                candiesPer[n] = max(candiesPer[n+1] + 1, candiesPer[n])
-            
-        return sum(candiesPer)class Solution:
-    def candy(self, ratings: List[int]) -> int:
-        # sort the ratings
-        # start with +1 for each space 
-        candiesPer = [1 for x in ratings]
-        N = len(ratings)
-        for n in range(1, N):
-            # if rating is greater than
-            if ratings[n]  > ratings[n-1]:
-                candiesPer[n] = max(candiesPer[n-1] + 1, candiesPer[n])
-        for n in range(N-2, -1, -1):
-            if ratings[n]  > ratings[n+1]:
-                candiesPer[n] = max(candiesPer[n+1] + 1, candiesPer[n])
-            
-        return sum(candiesPer)class Solution:
-    def candy(self, ratings: List[int]) -> int:
-        # sort the ratings
-        # start with +1 for each space 
-        candiesPer = [1 for x in ratings]
-        N = len(ratings)
-        for n in range(1, N):
-            # if rating is greater than
-            if ratings[n]  > ratings[n-1]:
-                candiesPer[n] = max(candiesPer[n-1] + 1, candiesPer[n])
-        for n in range(N-2, -1, -1):
-            if ratings[n]  > ratings[n+1]:
-                candiesPer[n] = max(candiesPer[n+1] + 1, candiesPer[n])
-            
-        return sum(candiesPer)class Solution:
-    def candy(self, ratings: List[int]) -> int:
-        # sort the ratings
-        # start with +1 for each space 
-        candiesPer = [1 for x in ratings]
-        N = len(ratings)
-        for n in range(1, N):
-            # if rating is greater than
-            if ratings[n]  > ratings[n-1]:
-                candiesPer[n] = max(candiesPer[n-1] + 1, candiesPer[n])
-        for n in range(N-2, -1, -1):
+        for n in range(len(ratings)-2, -1, -1):
             if ratings[n]  > ratings[n+1]:
                 candiesPer[n] = max(candiesPer[n+1] + 1, candiesPer[n])
             
